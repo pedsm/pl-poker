cf96112d5c971a18e11265720fc8f45c9a795888
Pedro Mendonca <peumendonca@gmail.com>
2022-09-19 01:56:08 +0100

    Potential start for a dark mode but chakra isn't that great

--- frontend/src/components/TableCard.vue
+++ frontend/src/components/TableCard.vue
@@ -1,45 +1,56 @@
 <template>
+  <c-light-mode>
     <div :key="member.id">
-        <c-box class="animate__animated animate__flipInX" :class="member.hidden || 'animate__tada'" backgroundColor="white" animationDuration="1s" w="90px" shadow="md" rounded="lg" p="5">
-            <c-text paddingY="30px" fontSize="xl" fontWeight="bold" align="center">
-                <span style="color:white" v-if="member.hidden">
-                    _ 
-                </span>
-                <span v-else>
-                    {{$store.getters.deck[member.card]}}
-                </span>
-            </c-text>
-        </c-box>
-        <c-text marginTop="1em">
-            <i class="fa fa-user"></i>
-            <span :class="member.id !== me.id || 'b'">
-                {{member.name}}
-            </span>
+      <c-box
+        class="animate__animated animate__flipInX"
+        :class="member.hidden || 'animate__tada'"
+        backgroundColor="white"
+        animationDuration="1s"
+        w="90px"
+        shadow="md"
+        rounded="lg"
+        p="5"
+      >
+        <c-text paddingY="30px" fontSize="xl" fontWeight="bold" align="center">
+          <c-text style="color: white" v-if="member.hidden"> _ </c-text>
+          <c-text style="color: black" v-else>
+            {{ $store.getters.deck[member.card] }}
+          </c-text>
         </c-text>
+      </c-box>
+      <c-text marginTop="1em">
+        <i class="fa fa-user"></i>
+        <span :class="member.id !== me.id || 'b'">
+          {{ member.name }}
+        </span>
+      </c-text>
     </div>
+  </c-light-mode>
 </template>
 <script>
-import { CBox, CText } from "@chakra-ui/vue"
+import { CBox, CText, CLightMode } from "@chakra-ui/vue";
 
 export default {
-    name: 'TableCard',
-    components: {
-        CBox,
-        CText,
+  name: "TableCard",
+  components: {
+    CBox,
+    CText,
+    CLightMode,
+  },
+  props: ["member"],
+  computed: {
+    me() {
+      return this.$store.getters.members.find(
+        (mem) => mem.id == this.$socket.id
+      );
     },
-    props: ['member'],
-    computed: {
-        me() {
-            return this.$store.getters.members
-                .find(mem => mem.id == this.$socket.id)
-        },
-    },
-}
+  },
+};
 </script>
 
 <style scoped>
 .mini {
-    margin: 0px auto;
-    font-size: 1em !important;
+  margin: 0px auto;
+  font-size: 1em !important;
 }
 </style>
