29927f4bce12ee3ae62ac3966131839a0043df9b
Pedro <pedrom@TKJX571C9D.local>
2023-04-11 15:40:18 +0100

    no build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-npm install -g @vue/cli
-rm -r dist | true
-pushd frontend
-npm ci
-npm run build
-mv ./dist ../
-popd
+# npm install -g @vue/cli
+# rm -r dist | true
+# pushd frontend
+# npm ci
+# npm run build
+# mv ./dist ../
+# popd
