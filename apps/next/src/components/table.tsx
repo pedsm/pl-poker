9e28d373b7038876ab87236bd4980cda9a843507
Pedro Santos de Mendonca <peumendonca@gmail.com>
2024-01-21 23:24:27 +0000

    90% there

--- apps/next/src/components/table.tsx
+++ apps/next/src/components/table.tsx
@@ -6,19 +6,27 @@ interface TableProps {
 }
 
 export default function Table(props: TableProps) {
-	const { members } = props.room
+	const { members, selectedDeck, availableDecks } = props.room
 	const cardsOnTable = members
 		.filter(m => m.card != null)
 
+	const deck = availableDecks[selectedDeck]
+
 	return (
 		<div>
-			<div className="flex justify-around w-full my-auto">
-				{cardsOnTable.map((card, i) => (
-					<div className="text-center" key={i}>
-						<DeckCard card={card.hidden ? '' : card?.card?.toString() ?? ''} />
-						<p className="mt-2">{card.name}</p>
-					</div>
-				))}
+			<div className="flex justify-around w-full h-full">
+				{cardsOnTable.map((card, i) => {
+					const index = card?.card ?? 0
+					const cardValue = deck.cards[index]
+				
+					return (
+							<div className="text-center my-auto" key={i}>
+								<DeckCard card={card.hidden ? '' : cardValue} />
+								<p className="mt-2">{card.name}</p>
+							</div>
+						)
+					})
+				}
 			</div>
 		</div>
 	);
