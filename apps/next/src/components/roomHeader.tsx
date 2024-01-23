6b68418213c28e9a75ff47258e4043453e7bffdb
Pedro Santos de Mendonca <peumendonca@gmail.com>
2024-01-23 22:45:41 +0000

    Tooltip

--- apps/next/src/components/roomHeader.tsx
+++ apps/next/src/components/roomHeader.tsx
@@ -5,6 +5,7 @@ import RoomSettings from "./roomSettings";
 import { Input } from "./ui/input";
 import RoomList from "./roomList";
 import { Link2Icon } from "@radix-ui/react-icons";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 interface RoomHeaderProps {
 	socket: ReturnType<typeof useSocket>;
@@ -18,15 +19,24 @@ export default function RoomHeader(props: RoomHeaderProps) {
 
 	const copyToClipboard = () => {
 		navigator.clipboard.writeText(window.location.href);
-		toast.info('Room url Copied to clipboard.')
+		toast.info('Room link Copied to clipboard.')
 	}
 
 	return (
 		<div>
 			<div className='w-full grid grid-cols-[auto_200px_min-content] gap-2 py-2'>
-				<h1 className='font-bold text-lg my-auto cursor-pointer' onClick={copyToClipboard}>
-					{room?.id ?? 'Loading...'} <Link2Icon className='inline' />
-				</h1>
+				<TooltipProvider>
+					<Tooltip delayDuration={100}>
+						<TooltipTrigger asChild>
+							<h1 className='font-bold text-lg my-auto cursor-pointer' onClick={copyToClipboard}>
+								{room?.id ?? 'Loading...'} <Link2Icon className='inline' />
+							</h1>
+						</TooltipTrigger>
+						<TooltipContent align="start" >
+							<p>Click to copy the room link to the clipboard.</p>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
 				<div>
 					<Input width={'200'} placeholder='Enter your name...' id="name" onChange={onChange}></Input>
 				</div>
