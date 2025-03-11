e5a9c08e34613a0dc7623fc2fa8b7d89a2916595
Pedro Mendonça <peumendonca@gmail.com>
2025-03-11 10:58:06 +0000

    Improve ping timeout and remove dead code

--- apps/frontend/babel.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
--- apps/frontend/src/types.ts
+++
@@ -1,7 +0,0 @@
-interface InputTarget extends EventTarget {
-  value: string
-}
-
-export interface TargetEvent extends Event {
-  target: InputTarget
-}
